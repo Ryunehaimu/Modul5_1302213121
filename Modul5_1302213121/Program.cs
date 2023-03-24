950535e91ceaf4e15794c8cc9774cebd49d22e44
PC9-IFLAB <soulreaper2407@gmail.com>
2023-03-24 08:29:56 +0700

    menambahkan class SimpleDataBase

--- Modul5_1302213121/Program.cs
+++ Modul5_1302213121/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Runtime.CompilerServices;
+
+namespace Modul5_1302213121
+{
+    internal class Program
+    {
+        public class SimpleDataBase<T>
+        {
+            private List<T> storedData;
+            private List<DateTime> inputDates;
+
+            public SimpleDataBase()
+            {
+                storedData = new List<T>();
+                inputDates = new List<DateTime>();
+            }
+            public void AddNewData(T data) {
+                storedData.Add(data);
+                inputDates.Add(DateTime.Now);
+            }
+            public void PrintAllData()
+            {
+                for (int i = 0; i < storedData.Count; i++)
+                {
+                    Console.WriteLine("Data 1 berisi: "+storedData[i]+ ", yang disimpan pada waktu UTC: " + inputDates[i]+" AM");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            SimpleDataBase<float> data = new SimpleDataBase<float>();
+            data.AddNewData(12.0f);
+            data.AddNewData(34.0f);
+            data.AddNewData(56.0f);
+            data.PrintAllData();
+        }
+    }
+}
