fd988df8049852e123d7394ad6186d99892c375a
PC9-IFLAB <soulreaper2407@gmail.com>
2023-03-24 08:07:32 +0700

    “menambahkan class Penjumlahan dengan method JumlahTigaAngka”

--- Modul5_1302213121/Program.cs
+++ Modul5_1302213121/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace Modul5_1302213121
+{
+    internal class Program
+    {
+        public class Penjumlahan
+        {
+            public static void JumlahTigaAngka<T>(T a, T b, T c)
+            {
+                dynamic penjumlahan = (dynamic)a + (dynamic)b + (dynamic)c;
+                Console.WriteLine("Hasil Penjumlahan: " + penjumlahan);
+            }
+        }
+        static void Main(String[] args)
+        {
+            //1302213121
+            Penjumlahan.JumlahTigaAngka<float>(13, 02, 21);
+        }
+    }
+}
+
+
+
